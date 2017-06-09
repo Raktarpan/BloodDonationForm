63a98b29e4fbe9d4375c1fd5b2fb77884a3c9695
Hrishikesh <hrishirt@cse.iitk.ac.in>
2017-06-10 04:31:48 +0530

    SQL / PHP initiation

---
+++ post.php
@@ -0,0 +1,80 @@
+<title>Thank You!</title>
+<?php
+
+require_once('config.php');
+
+function sane($var) {
+    return (isset($_REQUEST[$var]) ? (($_REQUEST[$var] != "") ? $_REQUEST[$var] : NULL) : NULL); 
+}
+
+$salt = "Raktarpan";
+$safety = array(md5($salt.'Hrishikesh'), md5($salt.'BloodDonationCamp'), md5($salt.'Shubhangi'));
+
+if (! in_array(sane('auth'), $safety)) {
+    die('<title>Denied</title><h1>Unauthorized Access</h1>');
+}
+
+$input = array ( 
+		"Roll" => sane('roll'),
+        "Name" => sane('name'),
+		"Email" => sane('email'),
+		"HostelAddr" => sane('addr'),
+		"Gender" => sane('gender'),
+		"Program" => sane('prog'),
+		"Dept" => sane('dept'),
+		"BloodGroup" => sane('bg'),
+//		"Category" => sane('cat'),
+		"Country" => sane('country')
+	       );
+
+// filter out NULL elements
+$input = array_filter($input);
+
+if (sizeof($input) == 0) {
+    echo "[]";
+    exit(0);
+}
+
+// build query
+
+$field = "CampId, Name, Gender, Phone, Age, BloodGroup, Address, Email, Source";
+$value_array = array();
+
+$query = "INSER INTO `donor` (".$field.") VALUES (".$values.")";
+
+$output = DB::query($query,$input);
+
+?>
+<h1>Thank You!</h1>
+
+Congratulations <? echo $Donor; ?>!<br>
+You are about to save three lives!<br>
+
+<hr>
+
+<h2>Procedure</h2>
+<ol>
+    <li>Please write your Name and Mobile Number on the paper form.</li>
+    <li>Carry this form with you and proceed for check-up.</li>
+    <li>Once check-up is done and you are cleared for donation, you will be given a blood-bag.</li>
+    <li>If there is a queue, please wait in the queue till your turn</li>
+    <li>Proceed for donation. It will take roughly 10-15 minutes</li>
+    <li>After donation, please continue to lie down for 5-10 more minutes.</li>
+    <li>You will now be provided refreshments.</li>
+    <li><b>Great Job! You're a hero!</b></li>
+</ol>
+
+<hr>
+
+<h2>Helpline</h2>
+
+We run a 24x7 helpline to provide blood to the needy. If you or your friends are ever in need of blood, feel free to call on this and we will try our best to help you out.
+
+<h4>+91 8882 982 982</h4>
+
+~ Raktarpan Team
+
+<hr>
+
+<a href="http://students.iitk.ac.in/raktarpan/camp/donate.php">Form</a>
+
